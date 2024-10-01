fc90257b9dad94588545d783c6ad02e66262530e
MaryamTarek03 <mariamtarek2233@gmail.com>
2024-10-01 19:42:18 +0300

    Customer Controller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using E_Commerce.Interfaces;
+using E_Commerce.Models.DbModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CustomerController : Controller
+    {
+        ICustomerRepo _customerRepo;
+        public CustomerController(ICustomerRepo customerRepo) => _customerRepo = customerRepo;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // data views
+        public IActionResult GetAll()
+        {
+            ICollection<Customer> customers = _customerRepo.GetAll();
+            return View(customers);
+        }
+        public IActionResult GetById(int id)
+        {
+            Customer? customer = _customerRepo.GetById(id);
+            return View(customer);
+        }
+
+        // form views
+        public IActionResult AddForm() // sign up maybe? you know what scrap the authentication part
+        {
+            return View();
+        }
+        public IActionResult EditForm(int id)
+        {
+            Customer? customer = _customerRepo.GetById(id);
+            return View(customer);
+        }
+
+        // functions
+        public IActionResult Add(Customer customer)
+        {
+            _customerRepo.Insert(customer);
+            return View("GetAll");
+        }
+        public IActionResult Edit(Customer customer)
+        {
+            _customerRepo.Update(customer);
+            return View("GetAll");
+        }
+        public IActionResult Delete(int id)
+        {
+            Customer? customer = _customerRepo.GetById(id);
+            _customerRepo.Delete(customer);
+            return View("GetAll");
+        }
+    }
+}
